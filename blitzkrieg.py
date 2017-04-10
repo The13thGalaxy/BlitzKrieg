5e93fa4c28138083fe85aa8405cc6cc230924025
Maya Pharis <borderlandsfangirl@gmail.com>
2017-04-10 09:36:39 -0500

    Update BlitzKrieg, imports.
    Updated blitzkrieg.py, as well as imported the needed .txt files for the random hero, weapon, etc. commands.

--- blitzkrieg.py
+++ blitzkrieg.py
@@ -1,4 +1,4 @@
-#BlitzKrieg
+#BlitzKrieg v.0.0.1
 #Copyright 2017 Maya Pharis
 
 #Imports and Bot Command definition
@@ -11,7 +11,6 @@ import random
 import sys
 from discord import utils
 from discord.ext.commands import Bot
-from discord.voice_client import VoiceClient
 logging.basicConfig(level = logging.INFO)
 
 #Definition of "%"
@@ -31,13 +30,13 @@ async def hello(*args):
 #Command: Ping
 @my_bot.command()
 async def ping(*args):
-	return await my_bot.say("I'm not saying it.")
+	return await my_bot.say("Leave me alone, nerd.")
 	#I thought some humor would be good here. Why not give BlitzKrieg his own personality, right?
 
 #Command: Random Operator (Rainbow Six Siege) - Attack
 @my_bot.command()
 async def attack():
-	op = random.randint(1, 76)
+	op = random.randint(1, 84)
 	infile = open("attackers.txt", "r")
 	line = infile.readline()
 	c = 1
@@ -49,5 +48,41 @@ async def attack():
 		await my_bot.say("You will be playing " + operator)
 	infile.close()
 
-#TODO: Run tests of the written code tonight (4/7). Debug as needed.
+#Command: Random Operator (Rainbow Six Siege) - Defense
+@my_bot.command()
+async def defense():
+	op = random.randint(1,76)
+	infile = open("defenders.txt", "r")
+	line = infile.readline()
+	c = 1
+	while c != op:
+		c = c + 1
+		line = infile.readline()
+	if c == op:
+		operator = line
+		await my_bot.say("You will be playing " + operator)
+	infile.close()
+	#There must be a more efficent way to randomly select an operator..God knows I'm too lazy too find it, though.
+
+#Command: Random Hero (Overwatch)
+@my_bot.command()
+async def hero():
+	hero = random.randint(1,25)
+	infile = open("heroes.txt", "r")
+	line = infile.readline()
+	c = 1
+	while c != hero:
+		c = c +1
+		line = infile.readline()
+	if c == hero:
+		herochoice = line
+		await my_bot.say("You will be playing " + herochoice)
+
+```
+#Command: Random Weapon (CS:GO)
+@my_bot.command()
+async def weapon():
+	wep = random .randint(1,23)
+```
+#TODO: Debug the progress from 0.0.1, then finish writing the random weapon code.
 my_bot.run(token)

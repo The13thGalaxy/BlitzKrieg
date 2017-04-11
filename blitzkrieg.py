2d4f5eb44560d4190c49c895516d821dad4d60c4
Laura <ikaheishi@ikaheishi.net>
2017-04-11 00:00:37 -0700

    Move data files to new data/ directory.
    And hopefully teach The13thGalaxy some organization skills as well.

--- blitzkrieg.py
+++ blitzkrieg.py
@@ -37,7 +37,7 @@ async def ping(*args):
 @my_bot.command()
 async def attack():
 	op = random.randint(1, 84)
-	infile = open("attackers.txt", "r")
+	infile = open("data/attackers.txt", "r")
 	line = infile.readline()
 	c = 1
 	while c != op:
@@ -52,7 +52,7 @@ async def attack():
 @my_bot.command()
 async def defense():
 	op = random.randint(1,76)
-	infile = open("defenders.txt", "r")
+	infile = open("data/defenders.txt", "r")
 	line = infile.readline()
 	c = 1
 	while c != op:
@@ -68,7 +68,7 @@ async def defense():
 @my_bot.command()
 async def hero():
 	hero = random.randint(1,25)
-	infile = open("heroes.txt", "r")
+	infile = open("data/heroes.txt", "r")
 	line = infile.readline()
 	c = 1
 	while c != hero:
